e3c1c85ba46699038eeb97c1704edea7ddcb6a6d
Zhaolin Lau <zhaolinlau@gmail.com>
2024-08-06 22:35:11 +0800

    add logout

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    
-  </div>
+	{{ user.email }}
+	<VBtn @click="logout" color="error" text="Logout" />
 </template>
 
 <script setup>
+const user = useSupabaseUser()
+const client = useSupabaseClient()
 
+const logout = async () => {
+	const { error } = await client.auth.signOut()
+	if (error) showError(error.message)
+	await navigateTo('/login')
+}
 </script>
-
-<style>
-
-</style>
