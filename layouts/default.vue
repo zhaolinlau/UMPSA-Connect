7db32d4577700c502d94a24f2ba07226432392b6
Zhaolin Lau <zhaolinlau@gmail.com>
2024-08-07 23:33:42 +0800

    add simple layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <v-layout>
+		<v-app-bar title="Application bar"></v-app-bar>
+		<v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-main>
 			<slot />
 		</v-main>
--- pages/login.vue
+++ pages/login.vue
@@ -1,51 +1,55 @@
 <template>
 	<div class="bg-teal-accent-1 fill-height">
-		<v-container class="fill-height" >
-		<v-row class="h-100">
-			<v-col md="6" class="hidden-sm-and-down">
+		<v-container class="fill-height">
+			<v-row class="h-100">
+				<v-col md="6" class="hidden-sm-and-down">
 
-			</v-col>
+				</v-col>
 
-			<v-col cols="12" md="6" class="elevation-10 bg-white rounded h-100">
-				<v-container class="fill-height">
-					<v-row class="mx-md-16">
-						<v-col cols="12">
-							<p class="text-h3">
-								Welcome back!
-							</p>
+				<v-col cols="12" md="6" class="elevation-10 bg-white rounded h-100">
+					<v-container class="fill-height">
+						<v-row class="mx-md-16">
+							<v-col cols="12">
+								<p class="text-h3">
+									Welcome back!
+								</p>
 
-							<p class="text-subtitle-1">
-								Stay connected with your campus faster and smarter
-							</p>
-						</v-col>
-						<v-col cols="12">
-							<VAlert @click:close="loginAlert = false" v-model="loginAlert" border="start" color="error"
-								variant="tonal" border-color="error" title="Error" icon="$error" :text="loginError" v-if="loginAlert"
-								closable />
-						</v-col>
-						<v-col cols="12">
-							<v-form @submit.prevent="login">
+								<p class="text-subtitle-1">
+									Stay connected with your campus faster and smarter
+								</p>
+							</v-col>
+							<v-col cols="12">
+								<VAlert @click:close="loginAlert = false" v-model="loginAlert" border="start" color="error"
+									variant="tonal" border-color="error" title="Error" icon="$error" :text="loginError" v-if="loginAlert"
+									closable />
+							</v-col>
+							<v-col cols="12">
+								<v-form @submit.prevent="login">
 
-								<VTextField label="Email" placeholder="Enter your email address" v-model="email"
-									prepend-inner-icon="i-mdi:email-outline" type="email" />
+									<VTextField label="Email" placeholder="you@example.com" v-model="email"
+										prepend-inner-icon="i-mdi:email-outline" type="email" />
 
-								<VTextField label="Password" v-model="password"
-									:append-inner-icon="visible ? 'i-mdi:eye-off' : 'i-mdi:eye'" :type="visible ? 'text' : 'password'"
-									placeholder="Enter your password" prepend-inner-icon="i-mdi:lock-outline"
-									@click:append-inner="visible = !visible" />
+									<VTextField label="Password" v-model="password"
+										:append-inner-icon="visible ? 'i-mdi:eye-off' : 'i-mdi:eye'" :type="visible ? 'text' : 'password'"
+										placeholder="example12345" prepend-inner-icon="i-mdi:lock-outline"
+										@click:append-inner="visible = !visible" />
 
-								<VBtn text="Login" type="submit" color="primary" block />
-							</v-form>
-						</v-col>
-					</v-row>
-				</v-container>
-			</v-col>
-		</v-row>
-	</v-container>
+									<VBtn text="Login" type="submit" color="primary" block />
+								</v-form>
+							</v-col>
+						</v-row>
+					</v-container>
+				</v-col>
+			</v-row>
+		</v-container>
 	</div>
 </template>
 
 <script setup>
+definePageMeta({
+	layout: false
+})
+
 const client = useSupabaseClient()
 const visible = ref(false)
 const password = ref('')
