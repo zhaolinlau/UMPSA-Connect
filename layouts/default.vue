f65dea489c8acec631b934581811a469ae12e13d
Zhaolin Lau <zhaolinlau@gmail.com>
2024-08-12 18:27:38 +0800

    add sidebar items

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,64 @@
+<script setup>
+const router = useRouter()
+const client = useSupabaseClient()
+
+const generalNavItems = ref(
+	{
+		General: [
+			{
+				title: 'Home',
+				to: '/'
+			},
+			{
+				title: 'Announcements',
+				to: '/announcements'
+			},
+			{
+				title: 'Events',
+				to: '/events'
+			},
+		],
+		Administration: [
+			{
+				title: 'Staffs',
+				to: '/staffs'
+			},
+			{
+				title: 'Students',
+				to: '/students'
+			},
+			{
+				title: 'Reports',
+				to: '/reports'
+			}
+		]
+	}
+)
+
+const logout = async () => {
+	const { error } = await client.auth.signOut()
+	if (error) showError(error.message)
+	await navigateTo('/login')
+}
+</script>
+
 <template>
 	<v-layout>
 		<v-app-bar title="UMPSA Connect"></v-app-bar>
 		<v-navigation-drawer>
 			<v-list>
-				<v-list-item title="Navigation drawer"></v-list-item>
+				<VListSubheader title="GENERAL" />
+				<v-list-item v-for="navItem in generalNavItems.General" :key="navItem" :title="navItem.title" :to="navItem.to"
+					:active="router.currentRoute.value.path == navItem.to ? true : false" color="primary" />
+				<VListSubheader title="ADMINISTRATION" />
+				<v-list-item v-for="navItem in generalNavItems.Administration" :key="navItem" :title="navItem.title"
+					:to="navItem.to" :active="router.currentRoute.value.path == navItem.to ? true : false" color="primary" />
 			</v-list>
+			<template #append>
+				<div class="pa-2">
+					<VBtn @click="logout" color="error" block text="Logout" />
+				</div>
+			</template>
 		</v-navigation-drawer>
 		<v-main>
 			<slot />
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <script setup>
 const user = useSupabaseUser()
-const client = useSupabaseClient()
-
-const logout = async () => {
-	const { error } = await client.auth.signOut()
-	if (error) showError(error.message)
-	await navigateTo('/login')
-}
 </script>
 <template>
-	{{ user.email }}
-	<VBtn @click="logout" color="error" text="Logout" />
+	{{ user.email }}	
 </template>
