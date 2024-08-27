52eeb71c4d57c84bc79b2bb960340d7d0d182a25
Zhaolin Lau <zhaolinlau@gmail.com>
2024-08-28 01:42:26 +0800

    add seo

--- app.vue
+++ app.vue
@@ -2,6 +2,15 @@
 useHead({
 	title: 'UMPSA Connect'
 })
+
+useSeoMeta({
+	title: 'UMPSA Connect',
+	ogTitle: 'UMPSA Connect',
+	description: 'UMPSA Connect serves as a comprehensive solution to enhance communication, streamline information dissemination, foster greater engagement, and better data security within the UMPSA student community. By empowering students with easy access to relevant information and facilitating seamless communication, the platform contributes to a more connected and informed campus environment.',
+	ogDescription: 'UMPSA Connect serves as a comprehensive solution to enhance communication, streamline information dissemination, foster greater engagement, and better data security within the UMPSA student community. By empowering students with easy access to relevant information and facilitating seamless communication, the platform contributes to a more connected and informed campus environment.',
+	ogImage: '/img/login.png',
+	twitterCard: 'summary_large_image',
+})
 </script>
 
 <template>
