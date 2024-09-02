a56493c25bc401d58ec0366251b5cb83e4f6b085
Zhaolin Lau <zhaolinlau@gmail.com>
2024-09-02 14:09:07 +0800

    fix image in supabase storage not being deleted

--- server/api/posts/index.delete.js
+++ server/api/posts/index.delete.js
@@ -3,6 +3,6 @@ import { serverSupabaseClient } from '#supabase/server'
 export default defineEventHandler(async (event) => {
 	const client = await serverSupabaseClient(event)
 	const body = await readBody(event)
-	await client.storage.from('images').remove([`posts/${body.post_id}/${body.post_media}`])
+	await client.storage.from('images').remove([`posts/${body.post_media}`])
 	await client.from('posts').delete().eq('id', body.post_id)
 })
