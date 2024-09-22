7c4111bbfe5282456b1d445a456e0ae2b19a6414
Zhaolin Lau <zhaolinlau@gmail.com>
2024-09-22 23:33:27 +0800

    Revert "fix gtag domain got rejected"
    This reverts commit 4aa20816df365da1a7bb73d878cc8de45656c7e0.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,8 +69,7 @@ export default defineNuxtConfig({
 		},
 		headers: {
 			contentSecurityPolicy: {
-				'img-src': ["'self'", 'data:', `${process.env.SUPABASE_URL}`],
-				'script-src': ["'self'", 'https:', "'unsafe-inline'", "'strict-dynamic'", "'nonce-{{nonce}}'", `${process.env.G_TAG_URL}`]
+				"img-src": ["'self'", "data:", `${process.env.SUPABASE_URL}`]
 			},
 			crossOriginEmbedderPolicy: process.env.NODE_ENV == 'development' ? 'unsafe-none' : 'credentialless'
 		}
