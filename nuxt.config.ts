4aa20816df365da1a7bb73d878cc8de45656c7e0
Zhaolin Lau <zhaolinlau@gmail.com>
2024-09-22 23:15:46 +0800

    fix gtag domain got rejected

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -69,7 +69,8 @@ export default defineNuxtConfig({
 		},
 		headers: {
 			contentSecurityPolicy: {
-				"img-src": ["'self'", "data:", `${process.env.SUPABASE_URL}`]
+				'img-src': ["'self'", 'data:', `${process.env.SUPABASE_URL}`],
+				'script-src': ["'self'", 'https:', "'unsafe-inline'", "'strict-dynamic'", "'nonce-{{nonce}}'", `${process.env.G_TAG_URL}`]
 			},
 			crossOriginEmbedderPolicy: process.env.NODE_ENV == 'development' ? 'unsafe-none' : 'credentialless'
 		}
