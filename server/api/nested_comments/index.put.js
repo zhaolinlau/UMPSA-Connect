00328968bac3678c76ce575f31f16356e1f86ef9
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 15:57:16 +0800

    add update nested comments backend

--- server/api/nested_comments/index.put.js
+++ server/api/nested_comments/index.put.js
@@ -1,3 +1,18 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('nested_comments').update({
+		content: body.content,
+		media: body.media
+	}).eq('id', body.id)
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
