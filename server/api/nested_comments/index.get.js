2b9baf2b160208e00b94f4299f84c7d950c5e85b
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 15:52:44 +0800

    add read nested comments backend

--- server/api/nested_comments/index.get.js
+++ server/api/nested_comments/index.get.js
@@ -1,3 +1,17 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const query = getQuery(event)
+
+	const { data, error } = await client.from('nested_comments').select('*').eq('comment_id', query.comment_id)
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	} else {
+		return data
+	}
 })
