a693a58941fd532729ccbaeee99beb81fc5e1e35
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 15:49:42 +0800

    add insert comment backend

--- server/api/nested_comments/index.post.js
+++ server/api/nested_comments/index.post.js
@@ -1,3 +1,21 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('nested_comments').insert([
+		{
+			comment_id: body.comment_id,
+			content: body.content,
+			media: body.media
+		}
+	])
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
