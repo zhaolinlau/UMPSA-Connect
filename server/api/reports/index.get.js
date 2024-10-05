801bd67a1412458f8b069593b9c0d8edb59b6a21
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 15:28:41 +0800

    add read reports backend

--- server/api/reports/index.get.js
+++ server/api/reports/index.get.js
@@ -1,3 +1,16 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+
+	const { data, error } = await client.from('reports').select('id, user_id, post_id, category, description, created_at')
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	} else {
+		return data
+	}
 })
