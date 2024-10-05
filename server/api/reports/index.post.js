997d12f55a2e8a98834c80ebb7f4a07367b148ba
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 15:11:33 +0800

    add insert report backend

--- server/api/reports/index.post.js
+++ server/api/reports/index.post.js
@@ -1,3 +1,23 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('reports').insert([
+		{
+			post_id: body.post_id,
+			comment_id: body.comment_id,
+			category: body.category,
+			description: body.description,
+			nested_comment_id: body.nested_comment_id
+		}
+	])
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
