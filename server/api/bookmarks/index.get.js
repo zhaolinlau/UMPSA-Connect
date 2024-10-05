1bec054bff76942da61845ec91959563a4057794
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 14:56:14 +0800

    add read bookmarks backend

--- server/api/bookmarks/index.get.js
+++ server/api/bookmarks/index.get.js
@@ -1,3 +1,41 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	if (body.post_id) {
+		const { data, error } = await client.from('bookmarks').select('id, post_id, created_at').eq('user_id', body.user_id)
+
+		if (error) {
+			throw createError({
+				statusCode: error.code,
+				statusMessage: error.message
+			})
+		} else {
+			return data
+		}
+	} else if (body.event_id) {
+		const { data, error } = await client.from('bookmarks').select('id, event_id, created_at').eq('user_id', body.user_id)
+
+		if (error) {
+			throw createError({
+				statusCode: error.code,
+				statusMessage: error.message
+			})
+		} else {
+			return data
+		}
+	} else if (body.announcement_id) {
+		const { data, error } = await client.from('bookmarks').select('id, announcement_id, created_at').eq('user_id', body.user_id)
+
+		if (error) {
+			throw createError({
+				statusCode: error.code,
+				statusMessage: error.message
+			})
+		} else {
+			return data
+		}
+	}
 })
