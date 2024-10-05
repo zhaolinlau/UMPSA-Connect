034ffb9dacb3a7befa286165c8dc2449ba93e469
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-05 13:36:03 +0800

    add insert bookmark backend

--- server/api/bookmarks/index.post.js
+++ server/api/bookmarks/index.post.js
@@ -1,3 +1,21 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('bookmarks').insert([
+		{
+			post_id: body.post_id,
+			event_id: body.event,
+			announcement_id: body.announcement_id
+		}
+	])
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
