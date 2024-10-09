f04f172e43347aa4eec0772df00b63be085deea2
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-09 10:37:31 +0800

    add insert announcement backend

--- server/api/announcements/index.post.js
+++ server/api/announcements/index.post.js
@@ -1,3 +1,22 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('announcements').insert([
+		{
+			title: body.title,
+			content: body.content,
+			media: body.media,
+			target_user: body.target_user
+		}
+	])
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
