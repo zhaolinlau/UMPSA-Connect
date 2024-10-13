45951d61e3480a53b313088653551fe8b6aa112c
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-13 12:04:05 +0800

    add update staff backend

---
+++ server/api/staffs/index.put.js
@@ -0,0 +1,23 @@
+import { serverSupabaseClient } from '#supabase/server'
+
+export default defineEventHandler(async (event) => {
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('staffs').update({
+		employee_id: body.employee_id,
+		name: body.name,
+		avatar: body.avatar,
+		gender: body.gender,
+		nationality: body.nationality,
+		department: body.department,
+		position: body.position
+	}).eq('user_id', body.user_id)
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
+})
