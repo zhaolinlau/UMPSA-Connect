4146b6d1728626a28d6af89e2f5d5c39e8775de2
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-13 12:13:29 +0800

    add delete user backend

--- server/api/users/index.delete.js
+++ server/api/users/index.delete.js
@@ -1,3 +1,15 @@
+import { serverSupabaseServiceRole } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = serverSupabaseServiceRole(event)
+	const body = await readBody(event)
+
+	const { error } = await client.auth.admin.deleteUser(body.id)
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
