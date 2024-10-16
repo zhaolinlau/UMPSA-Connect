cd491926b2cf5ae982da367d72d322ddb6b90222
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-16 16:04:15 +0800

    add update event backend

--- server/api/events/index.put.js
+++ server/api/events/index.put.js
@@ -1,3 +1,20 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('events').update({
+		title: body.title,
+		content: body.content,
+		media: body.media,
+		approval_status: body.approval_status
+	}).eq('id', body.id)
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
