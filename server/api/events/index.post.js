7191a9aaa978f542f4c20c62c5ca5053c83b5431
Zhaolin Lau <zhaolinlau@gmail.com>
2024-10-16 16:06:08 +0800

    add insert event backend

--- server/api/events/index.post.js
+++ server/api/events/index.post.js
@@ -1,3 +1,21 @@
+import { serverSupabaseClient } from '#supabase/server'
+
 export default defineEventHandler(async (event) => {
-  return 'Hello Nitro'
+	const client = await serverSupabaseClient(event)
+	const body = await readBody(event)
+
+	const { error } = await client.from('events').insert([
+		{
+			title: body.title,
+			content: body.content,
+			media: body.media
+		}
+	])
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	}
 })
