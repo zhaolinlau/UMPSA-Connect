d9a66fdca0c2f75b1156adeb5aadd0e8d449add3
Zhaolin Lau <zhaolinlau@gmail.com>
2024-11-05 22:02:18 +0800

    add read report frontend

--- pages/admin/reports.vue
+++ pages/admin/reports.vue
@@ -1,13 +1,22 @@
 <script setup>
+const { data: reports } = await useLazyFetch('/api/reports', {
+	method: 'GET',
+	transform: (reports) => {
+		return reports.map(report => (
+			{
+				category: report.category,
+				description: report.description,
+				'Created At': new Date(report.created_at).toLocaleString('en-GB', { timeZone: 'Asia/Kuala_Lumpur', hour12: true })
+			}
+		))
+	}
+})
 
+const search = ref('')
 </script>
 
 <template>
-  <div>
-    
-  </div>
+	<VTextField v-model="search" prepend-inner-icon="i-mdi:magnify" label="Search" flat single-line hide-details
+		variant="solo-filled" />
+	<VDataTable :items="reports" :search="search" />
 </template>
-
-<style>
-
-</style>
