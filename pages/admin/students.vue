76eaf874ed9ff72a735b1a2c3f5a287e82a54bb5
Zhaolin Lau <zhaolinlau@gmail.com>
2024-12-03 03:55:29 +0800

    link backend to students page

--- pages/admin/students.vue
+++ pages/admin/students.vue
@@ -1,13 +1,13 @@
 <script setup>
+const { data: students } = await useFetch('/api/students', {
+	method: 'get',
+})
 
+const search = ref('')
 </script>
 
 <template>
-  <div>
-    
-  </div>
+	<VTextField v-model="search" prepend-inner-icon="i-mdi:magnify" label="Search" flat single-line hide-details
+		variant="solo-filled" />
+	<VDataTable :items="students" :search="search" />
 </template>
-
-<style>
-
-</style>
