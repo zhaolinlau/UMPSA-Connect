f772fa9d9594eae6d85534867e45832384de285e
Zhaolin Lau <zhaolinlau@gmail.com>
2024-12-03 03:55:18 +0800

    link backend to staffs page

--- pages/admin/staffs.vue
+++ pages/admin/staffs.vue
@@ -1,13 +1,13 @@
 <script setup>
+const { data: staffs } = await useFetch('/api/staffs', {
+	method: 'get',
+})
 
+const search = ref('')
 </script>
 
 <template>
-  <div>
-    
-  </div>
+	<VTextField v-model="search" prepend-inner-icon="i-mdi:magnify" label="Search" flat single-line hide-details
+		variant="solo-filled" />
+	<VDataTable :items="staffs" :search="search" />
 </template>
-
-<style>
-
-</style>
