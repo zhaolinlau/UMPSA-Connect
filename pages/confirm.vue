91485e7c1a66ddaa44dbb3f970211e42428fabf4
Zhaolin Lau <zhaolinlau@gmail.com>
2024-12-24 00:24:52 +0800

    add auto insert profile, staff, and student when first time login

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -4,9 +4,56 @@ definePageMeta({
 })
 
 const user = useSupabaseUser()
+const client = useSupabaseClient()
+
+const addProfile = async (email) => {
+	const { data: profile, error } = await client.from('profiles').insert([
+		{
+			role: email.split('@')[1].toLocaleLowerCase() == 'adab.umpsa.edu.my' ? 'student' : 'staff'
+		}
+	]).select().single()	
+
+	if (error) {
+		throw createError({
+			statusCode: error.code,
+			statusMessage: error.message
+		})
+	} else {
+		if (profile.role == 'student') {
+			console.log(email.split('@')[0].toLocaleUpperCase())
+			const { error } = await client.from('students').insert([
+				{
+					matric_id: email.split('@')[0].toLocaleUpperCase(),
+					profile_id: profile.id
+				}
+			])
+
+			if (error) {
+				throw createError({
+					statusCode: error.code,
+					statusMessage: error.message
+				})
+			}
+		} else if (profile.role == 'staff') {
+			const { error } = await client.from('staffs').insert([
+				{
+					profile_id: profile.id
+				}
+			])
+
+			if (error) {
+				throw createError({
+					statusCode: error.code,
+					statusMessage: error.message
+				})
+			}
+		}
+	}
+}
 
 watch(user, async () => {
 	if (user.value) {
+		await addProfile(user.value.email)
 		await navigateTo('/')
 	} else {
 		await navigateTo('/login')
@@ -22,7 +69,7 @@ watch(user, async () => {
 			</p>
 		</vCol>
 		<vCol cols="12" lg="6">
-			<VProgressLinear rounded="" color="primary" indeterminate height="10" />
+			<VProgressLinear rounded color="primary" indeterminate height="10" />
 		</vCol>
 	</vRow>
 </template>
