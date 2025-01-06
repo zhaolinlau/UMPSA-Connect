c3deeb95152293c4cab94fe23da32eb31a8e86e5
Zhaolin Lau <zhaolinlau@gmail.com>
2025-01-06 17:36:58 +0800

    fix cannot access to faculty

--- server/api/notifications/index.get.js
+++ server/api/notifications/index.get.js
@@ -8,6 +8,10 @@ export default defineEventHandler(async (event) => {
 		const { data } = await client.from('notifications').select('*').eq('user_id', query.user_id).eq('announcement_id', query.announcement_id).single()
 
 		return data
+	} else if (query.count) {
+		const { data } = await client.from('notifications').select('*').eq('user_id', query.user_id).eq('read', false)
+
+		return data
 	} else {
 		const { data } = await client.from('notifications').select('*, announcements(*)').eq('user_id', query.user_id).order('created_at', { ascending: false })
 
