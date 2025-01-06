97d99dbba8cf3fff30cd5849065eebf0ab8fb33f
Zhaolin Lau <zhaolinlau@gmail.com>
2025-01-06 19:56:45 +0800

    adjust ai assistant layout

--- pages/ai_assistant.vue
+++ pages/ai_assistant.vue
@@ -15,44 +15,61 @@ const textRules = ref({
 })
 
 const sendText = async () => {
-	loading.value = true
-	const { valid } = await textFormRef.value.validate()
+	try {
+		loading.value = true
+		const { valid } = await textFormRef.value.validate()
 
-	if (valid) {
-		contents.value.push({
-			role: 'user',
-			parts: [{
-				text: textForm.text
-			}]
-		})
+		if (valid) {
+			contents.value.push({
+				role: 'user',
+				parts: [{
+					text: textForm.text
+				}]
+			})
 
-		textFormRef.value.reset()
+			textFormRef.value.reset()
 
-		const data = await $fetch('/api/gemini', {
-			method: 'post',
-			body: {
-				contents: contents.value
-			}
-		})
+			const data = await $fetch('/api/gemini', {
+				method: 'post',
+				body: {
+					contents: contents.value
+				}
+			})
 
-		contents.value.push(data)
+			contents.value.push(data)			
+		}
+
+	} catch (error) {
+		console.error(error.message)
+	} finally {
+		loading.value = false
 	}
-
-	loading.value = false
 }
 </script>
 
 <template>
-	<div v-for="content in contents" :key="content" class="mt-5">
-		<p>{{ content.role == 'user' ? 'You' : 'AI Assistant' }}:</p>
-		<div v-for="part in content.parts">
-			<VueMarkdown :markdown="part.text" />
-		</div>
-	</div>
+	<VContainer>
+		<VRow justify="center">
+			<template v-for="content in contents" :key="content">
+				<VCol cols="12" lg="7">
+					<VDivider />
+				</VCol>
+				<VCol cols="12" lg="7" :class="content.role == 'user' ? 'text-end' : ''">
+					<p :class="`font-weight-bold ${content.role == 'user' ? 'text-primary' : 'text-secondary'}`">
+						{{ content.role == 'user' ? 'You' : 'AI Assistant' }}:</p>
+					<div v-for="part in content.parts">
+						<VueMarkdown :markdown="part.text" />
+					</div>
+				</VCol>
+			</template>
 
-	<VForm @submit.prevent="sendText" ref="textFormRef">
-		<VTextField v-model="textForm.text" label="Text" :rules="textRules.text" :loading="loading" :disabled="loading"
-			class="mt-5" />
-		<VBtn text="Send" color="primary" variant="elevated" type="submit" :loading="loading" />
-	</VForm>
+			<VCol cols="12" lg="7">
+				<VForm @submit.prevent="sendText" ref="textFormRef">
+					<VTextarea v-model="textForm.text" label="Text" :rules="textRules.text" :loading="loading"
+						:disabled="loading" />
+					<VBtn text="Send" color="primary" variant="elevated" type="submit" :loading="loading" id="send_text" />
+				</VForm>
+			</VCol>
+		</VRow>
+	</VContainer>
 </template>
